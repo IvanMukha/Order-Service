a9720e800d796ada49d59f5f639d1a23200b4c15
IvanMukha <ivan.mukha228@gmail.com>
2026-08-10 02:02:12 +0300

    remove public modifier

--- src/test/java/com/mukha/orderservice/integrationtest/ItemControllerTest.java
+++ src/test/java/com/mukha/orderservice/integrationtest/ItemControllerTest.java
@@ -20,7 +20,7 @@ import static org.springframework.test.web.servlet.request.MockMvcRequestBuilder
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.jsonPath;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
 
-public class ItemControllerTest  extends AbstractIntegrationTest{
+class ItemControllerTest  extends AbstractIntegrationTest{
     @Autowired
     private MockMvc mockMvc;
 
